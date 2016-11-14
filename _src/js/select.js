705dfac8761f4c5d7a194974cf99f8213df8bb0b
dudusbarros <joseeduardobarros@gmail.com>
2016-11-14 12:22:32 -0200

    Create and improve Select js and css Add Tickets Add TicketsOption Add initial tests with user interface Add icon fonts

--- _src/js/select.js
+++ _src/js/select.js
@@ -1,8 +1,3 @@
-/*!
- * Elbit Tô No Vaso Project v0.0.0 (http://elbit.com.br/)
- * Copyright 2013-2016 Elbit Developers
- * Licensed under MIT (https://github.com/elbitdigital/base/blob/master/LICENSE)
-*/
 /* Select */
 
 var Select = (function () {
