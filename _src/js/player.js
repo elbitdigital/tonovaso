74fd15495b972a9f277db35c4f11dbf83c4bed31
dudusbarros <joseeduardobarros@gmail.com>
2016-12-01 11:43:07 -0200

    Add Player Add YouTubeVideo

---
+++ _src/js/player.js
@@ -0,0 +1,127 @@
+
+/* Player */
+
+var Player = (function () {
+
+	/**
+	 * Player constructor
+	 * @constructor
+	 */
+	function Player(element) {
+
+		var self = this;
+
+		this.element = element;
+		this.video = false;
+
+		this.pause = function () {
+
+			if (self.video)	self.video.pause();
+
+		};
+
+		this.play = function () {
+
+			if (self.video)	self.video.play();
+
+		};
+
+		this.stop = function () {
+
+			if (self.video)	self.video.stop();
+
+		};
+
+		this.power = function () {
+
+			var tag = document.createElement('script');
+
+			tag.src = "https://www.youtube.com/iframe_api";
+			var firstScriptTag = document.getElementsByTagName('script')[0];
+			firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+		};
+
+		if (this.element)
+			this.init();
+
+	}
+
+	Player.prototype.setVideo = function (video) {
+
+		this.video = video;
+		this.element.style.backgroundImage = 'url(http://i.ytimg.com/vi/' + this.video.id + '/hqdefault.jpg)';
+
+	};
+
+	Player.prototype.addListeners = function () {
+
+		var self = this;
+
+		try {
+
+			this.element.addEventListener('click', self.power);
+
+		} catch (e) { }
+
+	};
+
+	/**
+	 * This function add a control listener to some button (controlElement)
+	 * The listener is based on 'type' param
+	 * The acceptable values to 'type' param is: play, pause and stop
+	 * The default value is 'play'
+	 * @param type
+	 * @param controlElement
+	 */
+	Player.prototype.addControl = function (type, controlElement) {
+
+		var self = this;
+
+		switch (type) {
+
+			case 'pause':
+				self.addControlListener(controlElement, self.pause);
+				break;
+			case 'play':
+				self.addControlListener(controlElement, self.play);
+				break;
+			case 'stop':
+				self.addControlListener(controlElement, self.stop);
+				break;
+			default:
+				self.addControlListener(controlElement, self.play);
+
+		}
+
+	};
+
+	/**
+	 * It's a advanced function to add a event listener to control element
+	 * @param controlElement
+	 * @param controlListener
+	 */
+	Player.prototype.addControlListener = function (controlElement, controlListener) {
+
+		try {
+
+			controlElement.addEventListener('click', controlListener);
+
+		} catch (e) {
+
+			if (controlElement)
+				controlElement.attachEvent('onclick', controlListener);
+
+		}
+
+	};
+
+	Player.prototype.init = function () {
+
+		this.addListeners();
+
+	};
+
+	return Player;
+
+})();
---
+++ _src/js/youtubevideo.js
@@ -0,0 +1,112 @@
+
+/* YouTubeVideo */
+
+var YouTubeVideo = (function () {
+
+	/**
+	 * YouTubeVideo constructor
+	 * @constructor
+	 */
+	function YouTubeVideo(id) {
+
+		var self = this;
+
+		this.id = id;
+		this.player = false;
+
+		window.onYouTubeIframeAPIReady = function() {
+
+			self.player = new YT.Player('player', self.config);
+
+		};
+
+		this.onPlayerReady = function(event) {
+
+			event.target.playVideo();
+
+			console.log(self.formatTime(self.getCurrentTime()));
+			setInterval(function () {
+
+				console.log(self.formatTime(self.getCurrentTime()));
+
+			}, 1000);
+
+		};
+
+		this.onPlayerStateChange = function(event) {
+
+//				if (event.data == YT.PlayerState.PLAYING && !done) {
+//						setTimeout(stopVideo, 6000);
+//						done = true;
+//				}
+
+		};
+
+		this.config = {
+			height: '100%',
+			width: '100%',
+			videoId: self.id,
+			playerVars: {
+				'autoplay': 0,
+				'color': 'white',
+				'disablekb': 1,
+				'iv_load_policy': 3,
+				'modestbranding': 1,
+				'rel': 0,
+				'showinfo': 0,
+				'theme': 'light'
+			},
+			events: {
+				'onReady': self.onPlayerReady,
+				'onStateChange': self.onPlayerStateChange
+			}
+		};
+
+	}
+
+	YouTubeVideo.prototype.pause = function () {
+
+		this.player.pauseVideo();
+
+	};
+
+	YouTubeVideo.prototype.play = function () {
+
+		this.player.playVideo();
+
+	};
+
+	YouTubeVideo.prototype.stop = function () {
+
+		this.player.stopVideo();
+
+	};
+
+	YouTubeVideo.prototype.formatTime = function(time) {
+
+		time = Math.round(time);
+
+		// get minutes and seconds
+		var minutes = Math.floor(time / 60);
+		var seconds = time - minutes * 60;
+		seconds = seconds < 10 ? '0' + seconds : seconds;
+
+		return minutes + ":" + seconds;
+
+	};
+
+	YouTubeVideo.prototype.getCurrentTime = function () {
+
+		return this.player.getCurrentTime()
+
+	};
+
+	YouTubeVideo.prototype.getDuration = function () {
+
+		return this.player.getDuration();
+
+	};
+
+	return YouTubeVideo;
+
+})();
